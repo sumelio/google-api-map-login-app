74a59783f8268690e313afe97b65943250d6b415
m <you@example.com>
2018-11-12 10:10:01 -0500

    Add backend + constant + Redux store, Provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { render } from 'react-dom'; 
- 
+import { render } from 'react-dom';
 import { App } from './App';
- 
+import { Provider } from 'react-redux';
+import { store, configureFakeBackend } from './backend/helpers';
 
-render( 
-        <App /> ,
+configureFakeBackend();
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.getElementById('app')
 );
