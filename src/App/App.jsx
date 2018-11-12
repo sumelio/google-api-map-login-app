4e81dc5e359121ee74906d2c800c18f31e869f3d
m <you@example.com>
2018-11-12 09:53:34 -0500

    Add modules,order folders and create webpack.config.js

---
+++ src/App/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class connectedApp extends React.Component {
+    constructor(props) {
+        super(props);
+ 
+    }
+
+    render() { 
+        return (
+            <div > 
+             Aca va el login
+            </div>
+        );
+    }
+}
+ 
+export { connectedApp as App }; 
