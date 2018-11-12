01233c8064d66d7345d5d8ee428280b550cf7b7c
m <you@example.com>
2018-11-12 10:29:03 -0500

    Add login pages

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { Router, Route } from 'react-router-dom';
+import { LoginPage } from '../components/LoginPage';
+import { history } from '../backend/helpers';
 
 class connectedApp extends React.Component {
     constructor(props) {
@@ -9,7 +12,18 @@ class connectedApp extends React.Component {
     render() { 
         return (
             <div > 
-             Aca va el login
+                <div className="container">
+                    <div className="col-sm-8 col-sm-offset-2">
+                        {alert.message &&
+                            <div className={`alert ${alert.type}`}>{alert.message}</div>
+                        }
+                        <Router history={history}>
+                            <div> 
+                                <Route path="/" component={LoginPage} /> 
+                            </div>
+                        </Router>
+                    </div>
+                </div>
             </div>
         );
     }
---
+++ src/components/LoginPage/LoginPage.jsx
@@ -0,0 +1,20 @@
+import React from 'react'; 
+ 
+
+class LoginPage extends React.Component {
+
+
+    render() { 
+        return (
+            <div className="col-md-6 col-md-offset-3">
+                <h2>Login</h2>
+                <form name="form"  > 
+                form Login
+                </form>
+            </div>
+        );
+    }
+}
+
+ 
+export default LoginPage; 
